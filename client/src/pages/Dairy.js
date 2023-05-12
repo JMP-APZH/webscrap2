be40f9b02cd06560c5f05de39ac5e32071d84b63
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2023-05-12 22:00:24 +0200

    styling product prez

--- client/src/pages/Dairy.js
+++ client/src/pages/Dairy.js
@@ -36,8 +36,8 @@ const Dairy = () => {
         <h1 className="text-yellow-400 text-center w-screen">Dairy</h1>
         <div className='flex flex-col items-center bg-blue-500 w-screen h-screen p-4'>
         <div>
-        <h1 className='text-center pb-6'>Dairy prices review</h1>
-        <div className='text-center bg-black text-white w-72 rounded-full mx-5'>
+        <h1 className='text-center'>Dairy prices review</h1>
+        <div className='text-center bg-black text-white w-72 rounded-full m-5'>
            <button onClick={handleClick}>Scrape Dairy</button>
            </div> 
         </div>
@@ -45,31 +45,10 @@ const Dairy = () => {
             <div>
           
       
-      <div className='grid grid-cols-2 bg-blue-500 h-auto'>
+      <div className='grid grid-cols-2 bg-white h-auto'>
       {data.map((item, index) => (
-        <div key={index} className="text-center p-4">
-          {/* <ul key={index}> */}
-            {/* <li> */}
-              <p className='text-center'> {item.nom} </p>
-              <div className='flex gap-6 justify-center'>
-                <img
-                  className=""
-                  src={item.img}
-                  alt={item.nom}
-                  width="110"
-                  height="110"
-                />
-              <div className='flex flex-col gap-3'>
-                <p> {item.prix} </p>
-                <p> {item.prixspecial} </p>
-                <p> {item.quantite2} </p>
-                <p> {item.prixunite} </p>
-                <p> {item.nutriscore} </p>
-                <p> {item.nutrifull} </p>
-              </div>
-              
-              </div>
-              <div className='flex justify-center bg-pink-400 w-24'>
+        <div key={index} className="text-center p-4 border m-2">
+        <div className='flex right-0 w-24 absolute '>
                 <img
                   className="p-4"
                   src={item.nutrifull}
@@ -77,6 +56,38 @@ const Dairy = () => {
                   height="150"
                 />
               </div>
+        <div className='flex gap-6 justify-center'>
+                <img
+                  className=""
+                  src={item.img}
+                  alt={item.nom}
+                  width="200"
+                  height="200"
+                />
+                
+                </div>
+                <div className='flex items-center'>
+                  <p className='text-center font-semibold px-4'> {item.nom} </p>
+                  <div>
+                    <p className='text-center font-semibold p-4 text-red-700 border border-blue-700 mb-1 text-xs'> {item.prix} </p>
+                    <p className='text-center font-semibold p-4 text-green-700 border border-red-700 text-xs'> {item.prixspecial} </p>
+                    <p className='text-xs'> {item.prixunite} </p>
+                  </div>
+                  
+                </div>
+              
+              
+              <div className='flex flex-row items-center'>
+                
+                <p className='text-blue-600 font-bold text-xs px-3'> {item.quantite2} </p>
+                
+                </div>
+                {/* <p> {item.nutriscore} </p> */}
+                {/* <p> {item.nutrifull} </p> */}
+              
+              
+              
+              
               
               
               
