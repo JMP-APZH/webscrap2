7db99531b20d4f44211fd846bd3ae2a1beb35e7c
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2023-05-19 00:15:14 +0200

    testing the new rendering of viandes & poissons scrap

--- server/routes/scrapall.js
+++ server/routes/scrapall.js
@@ -280,5 +280,58 @@ router.get('/scrapefruitslegumes', (req, res) => {
     });
     });
 
+    router.get('/scrapeviandespoissons', (req, res) => {
+
+
+      const url = 'http://localhost:3015/scrape-viandespoissons';
+      
+      // axios.get(url)
+      axios.get(url, {
+      headers: {
+          'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+      },
+      })
+      .then(response => {
+          const $ = cheerio.load(response.data);
+      
+          const viandespoissons = []
+      
+          $('div.productInList', response.data).each(function() {
+          const nom = $(this).find('a').attr('title')
+          const url = $(this).find('a').attr('href')
+          const prix = $(this).find('p.price-full').text()
+          const prixspecial = $(this).find('p.price-special').text()
+          const img = $(this).find('img.owl-lazy').attr('data-src')
+          const quantite = $(this).find('div.desc-small-text').text()
+          // const quantite2 = $(this).find('div.poids-suffixe-holder').text().replaceAll('\n', '').replaceAll('\t', '').replaceAll(' ', '')
+          const quantite2 = $(this).find('div.poids-suffixe-holder').text().replace(/\n|\t| /g, '');
+          // const prixunite = $(this).find('div.unity-price').text().replaceAll('\n', '').replaceAll('\t', '').replaceAll(' ', '')
+          const prixunite = $(this).find('div.unity-price').text().replace(/\n|\t| /g, '');
+          const nutriscore = $(this).find('div.picto-item').find('img').attr('src')
+          const web = 'https://martinique.123-click.com'
+          const nutrifull = web.concat(nutriscore)
+          viandespoissons.push({
+              nom,
+              prix,
+              prixspecial,
+              quantite,
+              quantite2,
+              prixunite,
+              img,
+              nutrifull,
+              nutriscore,
+              url,
+          })
+          })
+          console.log('articles from server:', viandespoissons)
+          // res.header('Access-Control-Allow-Origin', '*');
+          res.send(viandespoissons);
+          // res.send('Hello, world!');
+      }).catch(error => {
+          console.log(error);
+          res.send(error);
+      });
+      });
+
 
 module.exports = router
