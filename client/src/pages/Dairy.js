434df61dbf4d63352cf36878d9d7e85ad764a1b1
Jean-Marie Philocles <philocles.jeanmarie@gmail.com>
2023-05-18 23:26:12 +0200

    testing the new rendering of dairy scrap

--- client/src/pages/Dairy.js
+++ client/src/pages/Dairy.js
@@ -8,7 +8,7 @@ const Dairy = () => {
 
   const handleClick = () => {
     // axios.get('http://localhost:3010/scrape')
-    axios.get('https://webscrap972.onrender.com/scrape-dairy')
+    axios.get('https://webscrap972.onrender.com/scrapall/scrapedairy')
       .then(response => {
         console.log('data from response from client:', response.data)
         setData(response.data)
